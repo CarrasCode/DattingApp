ec62b973dad0cdff0ca8780e45f525694b315bfa
Halepensis <antgranada@gmail.com>
2026-01-16 17:59:19 +0100

    Añadido CustomUserManager para cambiar el username por el email, heredando de AbstractBaseUrl

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,16 +1,45 @@
 import uuid
 from datetime import date
 
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class CustomUser(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("El email es obligatorio"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=50)
+    email = models.EmailField(_("email address"), unique=True)
+
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(null=True, blank=True)
+
+    # Configuración para usar email como login
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return self.email
