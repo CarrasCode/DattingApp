b061e8ff57194b9e5a69f9847ce81253e30f33da
CarrasCode <antgranada@gmail.com>
2026-01-19 14:00:37 +0100

    Añadida la inscripción de modelos Profile y UserPhoto en el panel de administración

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from users.models import CustomUser
+from users.models import CustomUser, Profile, UserPhoto
 
-admin.site.register(CustomUser)
+admin.site.register([CustomUser, Profile, UserPhoto])
