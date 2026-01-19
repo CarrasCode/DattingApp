ea332d447c2e7ec65bb4df97ba88916791cc2da0
CarrasCode <antgranada@gmail.com>
2026-01-19 14:16:37 +0100

    Añadido la dependencia djangorestframework-simplejwt y configurada

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import ProfileViewSet, UserRegistrationView
 
@@ -14,6 +15,9 @@ router.register(r"profiles", ProfileViewSet, basename="profile")
 urlpatterns = [
     # Auth endpoints
     path("auth/register/", UserRegistrationView.as_view(), name="register"),
+    # --- NUEVOS ENDPOINTS PARA LOGIN (JWT) ---
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # Profile endpoints (generados por el router)
     path("", include(router.urls)),
 ]
