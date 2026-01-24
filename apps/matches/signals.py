20f82a5ada195bcbff7f962ceb1ed31f5f3c1e40
CarrasCode <antgranada@gmail.com>
2026-01-24 18:27:02 +0100

    Añadido manejo de señales para eliminar matches al bloquear usuarios

--- apps/matches/signals.py
+++ apps/matches/signals.py
@@ -1,8 +1,8 @@
 from django.db import transaction
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
-from .models import Match, Swipe
+from .models import Block, Match, Swipe
 
 
 @receiver(pre_delete, sender=Match)
@@ -24,3 +24,20 @@ def convert_likes_to_dislikes_on_unmatch(sender, instance: Match, **kwargs):
             Swipe.objects.filter(
                 swiper__in=[user_a, user_b], target__in=[user_a, user_b]
             ).update(value=Swipe.SwipeType.DISLIKE)
+
+
+@receiver(post_save, sender=Block)
+def delete_match_on_block(sender, instance: Block, created: bool, **kwargs):
+    """
+    Se ejecuta AUTOMÁTICAMENTE justo después de que un Block sea creado.
+    Borra cualquier Match existente entre el bloqueador y el bloqueado.
+    """
+    if created:
+        with transaction.atomic():
+            blocker = instance.blocker
+            blocked = instance.blocked
+
+            # Buscar y borrar cualquier Match entre blocker y blocked
+            Match.objects.filter(
+                user_a__in=[blocker, blocked], user_b__in=[blocker, blocked]
+            ).delete()
