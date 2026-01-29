e712961fb6410c3b5bc7f4d9e5e41f0b0a4b0479
CarrasCode <antgranada@gmail.com>
2026-01-29 16:41:17 +0100

    creadas interfaces de profiles

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,9 +1,51 @@
 export interface User {
-  id: number;
+  id: string;
   first_name: string;
-  age: number;
-  bio?: string;
-  work?: string;
-  distance_km?: number;
-  photos: { url: string }[];
+  bio: string | null;
+  work: string | null;
+  age: string;
+  gender: IGender;
+  photos: IPhoto[];
+  distance_km: string;
+}
+
+interface IGender {
+  gender: 'M' | 'F' | 'NB' | 'O';
+}
+interface IGenderPref {
+  gender: 'M' | 'F' | 'A';
+}
+export interface ICurrentProfile {
+  id: string;
+  first_name: string;
+  bio: string | null;
+  work: string | null;
+  age: string;
+  gender: IGender;
+  photos: IPhoto[];
+  email: string;
+  birth_date: string;
+  gender_preference: IGenderPref | null;
+  max_distance: number | null;
+  min_age: number | null;
+  max_age: number | null;
+  location: string;
+}
+export interface IEditProfile {
+  first_name: string;
+  bio: string | null;
+  work: string | null;
+  gender: IGender;
+  birth_date: string;
+  gender_preference: IGenderPref | null;
+  max_distance: number | null;
+  min_age: number | null;
+  max_age: number | null;
+}
+
+export interface IPhoto {
+  id: string;
+  image: string;
+  is_main: boolean | null;
+  caption: string | null;
 }
